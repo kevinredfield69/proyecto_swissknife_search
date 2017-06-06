7f5c16cef3293ec72a18284073f44ed7e24003de
Kevin <kevinsevillano9743@gmail.com>
2017-06-06 11:08:21 +0200

    Editando fichero del programa

--- programa.py
+++ programa.py
@@ -299,7 +299,7 @@ def twittear():
     if request.get_cookie("access_token", secret='some-secret-key'):
       TOKENS["access_token"]=request.get_cookie("access_token", secret='some-secret-key')
       TOKENS["access_token_secret"]=request.get_cookie("access_token_secret", secret='some-secret-key')
-      return template('tweet')
+      return template('/')
     else:
       redirect('/twitter')
 
